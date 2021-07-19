2786f7c57392bf80da10dcaa10fb7e5d53e859b0
Ascilator <yarmshuk.vlad@mail.ru>
2021-07-19 17:07:28 +0300

    changing server to localhost

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://github.com/scandiweb/junior-react-endpoint/tree/master/src",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
 

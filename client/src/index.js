34dfb998081699d8f74f24308ccc0a055bc0421e
Ascilator <yarmshuk.vlad@mail.ru>
2021-07-19 17:03:59 +0300

    for deploy

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://github.com/scandiweb/junior-react-endpoint/tree/master/src",
   cache: new InMemoryCache(),
 });
 
